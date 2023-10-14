f9621217d28d232436534b1f2f2e6c649aa20133
Abdessittir <harkati.web.dev@gmail.com>
2023-10-14 12:22:31 +0100

    Remove game context

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
-import GameProvider from './context/game.tsx';
 
 import SocketProvider from './context/socket.tsx';
 import App from './App.tsx';
@@ -10,10 +9,8 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GameProvider>
       <SocketProvider>
         <App />
       </SocketProvider>
-    </GameProvider>
   </React.StrictMode>,
 )
