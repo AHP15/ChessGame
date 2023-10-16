5dd8962dbbcee0c2ea5d56d4745566f898cbad17
Abdessittir <harkati.web.dev@gmail.com>
2023-10-16 13:28:40 +0100

    Add styles for alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,16 +2,15 @@ import { useState } from 'react';
 import styles from '../styles/Alert.module.css';
 
 const Alert = ({gameId}: { gameId: string }) => {
-    const [alert, setAlert] = useState(
-        () => `Copy the following game id: ${gameId} with the person you want to play with `
-    );
+    const [id, setId] = useState(gameId);
 
-    if(!alert) return null;
+    if(!id) return null;
 
     return (
         <div className={styles.alert}>
-            {alert}
-            <button onClick={() => setAlert('')} type='button'>Clear</button>
+            <p>Copy the following game id with the person you want to play: </p>
+            <span>{id}</span>
+            <button className="display_btn" onClick={() => setId('')} type='button'>Clear</button>
         </div>
     );
 };
