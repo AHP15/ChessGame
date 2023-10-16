0306f4032bff825f2d2a9d889480e0087532ca7e
Abdessittir <harkati.web.dev@gmail.com>
2023-10-16 17:34:37 +0100

    Create timers

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,25 @@ app.get('*', (req, res) => {
 
 const games = new Map();
 
+let timeForWhite;
+let secondsForWhite = 0;
+let timeForBlack;
+let secondsForBlack = 0;
+
+let timerForWhite;
+let timerForBlack;
+
+function generateTimer(callback) {
+    return setInterval(callback, 1000);
+}
+
 io.on('connection', (socket) => {
 
     socket.on('create-game', (game) => {
         console.log(game);
+        timeForWhite = game.time;
+        timeForBlack = game.time;
+
         games.set(game.id, game);
         socket.join(game.id);
     });
@@ -36,6 +51,8 @@ io.on('connection', (socket) => {
             return;
         }
 
+        let timerToStart;
+
         socket.join(info.gameId);
 
         if(editedGame.white.id) {
@@ -43,16 +60,74 @@ io.on('connection', (socket) => {
                 id: info.userId,
                 username: info.username,
             };
+            timerToStart = 'white';
         } else {
             editedGame.white = {
                 id: info.userId,
                 username: info.username,
             };
+            timerToStart = 'black';
         }
 
         games.set(editedGame.id, editedGame);
 
         io.to(info.gameId).emit('joined-game', editedGame);
+        /*
+        generateTimer(() => {
+            let white = timerToStart === 'white';
+            let time = {
+                seconds: white ? secondsForWhite : secondsForBlack,
+                minutes: white ? timeForWhite : timeForBlack,
+            };
+            
+            if(time.seconds <= 0) {
+                secondsForWhite = white ? 59: secondsForWhite;
+                secondsForBlack = !white? 59: secondsForBlack;
+
+                timeForWhite = white ? timeForWhite - 1: timeForWhite;
+                timeForBlack = !white ? timeForBlack - 1: timeForBlack;
+            } else {
+                secondsForWhite = white ? secondsForWhite - 1: secondsForWhite;
+                secondsForBlack = !white ? secondsForBlack - 1: secondsForBlack;
+            }
+
+            io.to(info.gameId).emit('time', {time, white});
+        });
+        */
+
+        if(timerToStart === 'white') {
+            timerForWhite = generateTimer(() => {
+                if(secondsForWhite <= 0) {
+                    secondsForWhite = 59;
+                    timeForWhite--;
+                } else {
+                    secondsForWhite--;
+                }
+
+                let time = {
+                    seconds: secondsForWhite,
+                    minutes: timeForWhite,
+                };
+
+                io.to(info.gameId).emit('time', {time, white: true});
+            });
+        } else {
+            timerForBlack = generateTimer(() => {
+                if(secondsForBlack <= 0) {
+                    secondsForBlack = 59;
+                    timeForBlack--;
+                } else {
+                    secondsForBlack--;
+                }
+
+                let time = {
+                    seconds: secondsForBlack,
+                    minutes: timeForBlack,
+                };
+
+                io.to(info.gameId).emit('time', {time, white: false});
+            });
+        }
     });
 
     socket.on('send-move', (info) => {
